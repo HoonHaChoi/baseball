bd53deada42a98c69bacf1d07112b13b2f70245c
ellyheetov <yaaajcm@naver.com>
2021-05-04 15:51:42 +0900

    feat: collectionView 구성 및 flip transition

--- iOS/BaseBall/BaseBall/GameListController.swift
+++ iOS/BaseBall/BaseBall/GameListController.swift
@@ -9,9 +9,45 @@ import UIKit
 
 class GameListController: UIViewController {
 
+    @IBOutlet weak var gameList: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerNIB()
+        gameList.delegate = self
+        gameList.dataSource = self
     }
     
+    func registerNIB() {
+        let nibName = UINib(nibName: "GameCell", bundle: nil)
+        gameList.register(nibName, forCellWithReuseIdentifier: "GameCell")
+        gameList.register(nibName, forCellWithReuseIdentifier: "GameOptionCell")
+    }
 }
 
+extension GameListController : UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! GameCell
+        cell.flip()
+    }
+}
+
+extension GameListController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = gameList.dequeueReusableCell(withReuseIdentifier: "GameCell", for: indexPath)
+        cell.layer.cornerRadius = 10
+        return cell
+    }
+}
+extension GameListController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: gameList.frame.width / 1.3, height: 80)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
---
+++ iOS/BaseBall/BaseBall/View/GameCell.swift
@@ -0,0 +1,41 @@
+//
+//  GameCell.swift
+//  BaseBall
+//
+//  Created by 박혜원 on 2021/05/04.
+//
+
+import UIKit
+
+class GameCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontView: UIStackView!
+    @IBOutlet weak var backView: UIStackView!
+    
+    var homeTeam: String = ""
+    var awayTeam: String = ""
+    
+    var isFaceUp: Bool = true
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func flip(){
+        if isFaceUp == true {
+            UIView.transition(from: frontView,
+                              to: backView,
+                              duration: 0.5,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion: nil)
+            self.contentView.backgroundColor = .white
+        } else {
+            UIView.transition(from: backView,
+                              to: frontView,
+                              duration: 0.5,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion: nil)
+        }
+        isFaceUp = !isFaceUp
+    }
+}
