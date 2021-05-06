8dcb34afa1fddf0ee98e46f22d0f07c043c197c9
hoonha choi <sosqsz2@naver.com>
2021-05-06 13:16:22 +0900

    feat: PlayHistoryHeaderView 생성

---
+++ iOS/BaseBall/BaseBall/View/PlayHistoryHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  PlayHistoryHeaderView.swift
+//  BaseBall
+//
+//  Created by HOONHA CHOI on 2021/05/06.
+//
+
+import UIKit
+
+class PlayHistoryHeaderView: UICollectionReusableView {
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static let nib: UINib = UINib(nibName: identifier, bundle: nil)
+    
+}
